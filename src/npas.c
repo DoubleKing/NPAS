01e7562aa99ae6ff2de43921b88471ecb685ddf5
doubleking <971774262@qq.com>
2018-08-02 17:36:48 +0800

    daemon

--- src/npas.c
+++ src/npas.c
@@ -5,12 +5,12 @@
 	> Created Time: Thu May  3 09:01:24 2018
  ************************************************************************/
 #include "npas_common.h"
-  
+
 #define BUFSIZE 1514
 #define FILE_NAME "xxx"
-bool write2file(char * filename, char * str)
+bool write2file(char *filename, char *str)
 {
-	if(filename==NULL && str == NULL)
+	if(filename == NULL && str == NULL)
 	{
 		return false;
 	}
@@ -20,7 +20,7 @@ bool write2file(char * filename, char * str)
 		printf("open fail\n");
 		return false;
 	}
-	char buf[64] = {0};
+	char buf[64] = { 0 };
 	time_t time_now;
 	time_now = time(&time_now);
 	strftime(buf, sizeof(buf), "%y-%m-%d %H:%M\t", gmtime(&time_now));
@@ -30,111 +30,104 @@ bool write2file(char * filename, char * str)
 
 	return true;
 }
-bool decode_tcp(const unsigned char * packet_buff, unsigned int len)
+
+bool decode_tcp(const unsigned char *packet_buff, unsigned int len)
 {
 	if(!packet_buff && len < sizeof(struct tcp_hdr))
 	{
 		return false;
 	}
-	struct tcp_hdr *tcp_hdr = (struct tcp_hdr *)packet_buff;
+	struct tcp_hdr *tcp_hdr = (struct tcp_hdr *) packet_buff;
 	if(ntohs(tcp_hdr->th_dport) == 80 || ntohs(tcp_hdr->th_sport) == 80)
 	{
-		char * host = strstr((char *)packet_buff + sizeof(struct tcp_hdr),"Host: ");
+		char *host = strstr((char *) packet_buff + sizeof(struct tcp_hdr), "Host: ");
 		if(host)
 		{
-			char * CF = strstr(host,"\r\n");
-			char ret[1024] = {0};
-			memcpy(ret, host+6, (CF - host - 6)>254 ? 254 :  CF - host -6);
-			char * payload = (char *)packet_buff + sizeof(struct tcp_hdr);
-			if(strncmp(payload,"GET ",4 ) == 0 || strncmp(payload,"POST", 4) == 0)
+			char *CF = strstr(host, "\r\n");
+			char ret[1024] = { 0 };
+			memcpy(ret, host + 6, (CF - host - 6) > 254 ? 254 : CF - host - 6);
+			char *payload = (char *) packet_buff + sizeof(struct tcp_hdr);
+			if(strncmp(payload, "GET ", 4) == 0 || strncmp(payload, "POST", 4) == 0)
 			{
-				char * end = strstr(payload,"HTTP");
+				char *end = strstr(payload, "HTTP");
 				if(end != NULL)
 				{
-					strncat(ret, payload+4, end-payload-4);
+					strncat(ret, payload + 4, end - payload - 4);
 				}
 			}
-			strcat(ret,"\n");
-			write2file(FILE_NAME,ret);
+			strcat(ret, "\n");
+			write2file(FILE_NAME, ret);
 		}
 
 	}
 	return true;
 }
 
-
-bool decode_ip(const unsigned char * packet_buff, unsigned int len)
+bool decode_ip(const unsigned char *packet_buff, unsigned int len)
 {
 	if(!packet_buff && len < 20)
 	{
 		return false;
 	}
-	struct ip_hdr * ip_hdr = (struct ip_hdr *)packet_buff;
-	//struct in_addr in;
-	//in.s_addr = ip_hdr->src_addr;
-	//printf("src_ip:%s\n",inet_ntoa(in));
-	//in.s_addr = ip_hdr->dst_addr;
-	//printf("dst_ip:%s\n",inet_ntoa(in));
+	struct ip_hdr *ip_hdr = (struct ip_hdr *) packet_buff;
 	if(ip_hdr->protocol == 0x06)
 	{
-		decode_tcp(packet_buff+sizeof(struct ip_hdr), len - sizeof(struct ip_hdr));
-	}
-	else
+		decode_tcp(packet_buff + sizeof(struct ip_hdr), len - sizeof(struct ip_hdr));
+	} else
 	{
 
 	}
 	return true;
 }
 
-/*******************************回调函数************************************/  
-void ethernet_protocol_callback(unsigned char *argument,const struct pcap_pkthdr *packet_header,const unsigned char *packet_content)  
-{ 
-    struct linux_cooked * linux_cooked;  
-    unsigned short protocol_type;
-    linux_cooked = (struct linux_cooked *)packet_content;  
-         
-    protocol_type = ntohs(linux_cooked->protocol_type);
-    //printf("Network type is :%04x\n",protocol_type);  
-    switch(protocol_type)  
-    {  
-        case 0x0800:
-		{
-			//printf("The network layer is IP protocol\n");
-			decode_ip(packet_content+sizeof(struct linux_cooked), packet_header->caplen - sizeof(struct linux_cooked));
-			break;//ip
-		}
-        case 0x0806:printf("The network layer is ARP protocol\n");break;//arp  
-        case 0x0835:printf("The network layer is RARP protocol\n");break;//rarp  
-        default:break;  
-    }
-    //usleep(800*1000);  
-}  
+void ethernet_protocol_callback(unsigned char *argument, const struct pcap_pkthdr *packet_header, const unsigned char *packet_content)
+{
+	struct linux_cooked *linux_cooked;
+	unsigned short protocol_type;
+	linux_cooked = (struct linux_cooked *) packet_content;
 
-int main(int argc, char *argv[])  
-{  
-	daemon(1,1);
-    char error_content[100];    //出错信息  
-    pcap_t * pcap_handle;  
-#if 0    
-    //获取网络接口  
-    net_interface = pcap_lookupdev(error_content);
-	printf("%s\n",net_interface);
-    if(NULL == net_interface)  
-    {  
-        perror("pcap_lookupdev");  
-        exit(-1);  
-    }  
-#endif
-    pcap_handle = pcap_open_live("venet0:0",BUFSIZE,1,0,error_content);//打开网络接口  
-	
-	int linktype = pcap_datalink(pcap_handle);
-    if(pcap_loop(pcap_handle,-1,ethernet_protocol_callback,(unsigned char*)&linktype) < 0)  
-    {  
-        perror("pcap_loop");  
-    }  
-      
-    pcap_close(pcap_handle);  
-    return 0;  
+	protocol_type = ntohs(linux_cooked->protocol_type);
+	switch (protocol_type)
+	{
+	case 0x0800:
+		{
+			decode_ip(packet_content + sizeof(struct linux_cooked), packet_header->caplen - sizeof(struct linux_cooked));
+			break;
+		}
+	case 0x0806:
+		printf("The network layer is ARP protocol\n");
+		break;
+	case 0x0835:
+		printf("The network layer is RARP protocol\n");
+		break;
+	default:
+		break;
+	}
+	//usleep(800*1000);  
 }
 
+int main(int argc, char *argv[])
+{
+	daemon(1, 1);
+	char error_content[100];  
+	pcap_t *pcap_handle;
+#if 0
+	net_interface = pcap_lookupdev(error_content);
+	printf("%s\n", net_interface);
+	if(NULL == net_interface)
+	{
+		perror("pcap_lookupdev");
+		exit(-1);
+	}
+#endif
+	pcap_handle = pcap_open_live("venet0:0", BUFSIZE, 1, 0, error_content);
 
+	int linktype = pcap_datalink(pcap_handle);
+	if(pcap_loop(pcap_handle, -1, ethernet_protocol_callback, (unsigned char *) &linktype) < 0)
+	{
+		perror("pcap_loop");
+	}
+
+	pcap_close(pcap_handle);
+	return 0;
+}
